bae7e3976cf146a82d24eb50a7d42cc4e6f81d97
Krozark <maxime.barbier1991@gmail.com>
2012-06-22 10:54:36 -0400

    add ContextNameField

--- custom-fields/fields/ContextNameField.py
+++ custom-fields/fields/ContextNameField.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db.models import CharField
 import re
 from django.forms.util import ValidationError
 from django.utils.translation import ugettext_lazy as _
